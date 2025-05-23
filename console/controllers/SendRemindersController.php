57b6afb0d0231f4d67ac534c8cd06578b863acd4
mwanginjamb <fnjambi@outlook.com>
2025-05-23 12:43:46 +0300

    recalibrate calculations for due appointments whose output is consumed by job queues

--- console/controllers/SendRemindersController.php
+++ console/controllers/SendRemindersController.php
@@ -5,6 +5,7 @@ use frontend\models\Appointments;
 use console\jobs\SendReminderEmailJob;
 use Yii;
 use yii\console\Controller;
+use yii\console\ExitCode;
 
 
 class SendRemindersController extends Controller
@@ -19,8 +20,13 @@ class SendRemindersController extends Controller
     {
         $appointments = Appointments::find()
             ->where(['reminder_5hrs_sent' => NULL])
-            ->andWhere('CONCAT(date, " ",time) > NOW()')
-            ->andWhere('DATE_SUB(CONCAT(date, " ",time), INTERVAL 5 HOUR) <= NOW()')
+            ->andWhere(['=', 'date', date('Y-m-d')])
+            ->andWhere([
+                'BETWEEN',
+                new \yii\db\Expression("STR_TO_DATE(CONCAT(date, ' ', time), '%Y-%m-%d %H:%i:%s')"),
+                new \yii\db\Expression("DATE_ADD(NOW(), INTERVAL 299 MINUTE)"), // 4h 59m
+                new \yii\db\Expression("DATE_ADD(NOW(), INTERVAL 301 MINUTE)")  // 5h 1m
+            ])
             ->all();
 
         foreach ($appointments as $appointment) {
@@ -38,8 +44,13 @@ class SendRemindersController extends Controller
     {
         $appointments = Appointments::find()
             ->where(['reminder_2hrs_sent' => NULL])
-            ->andWhere('CONCAT(date, " ",time) > NOW()')
-            ->andWhere('DATE_SUB(CONCAT(date, " ",time), INTERVAL 2 HOUR) <= NOW()')
+            ->andWhere(['=', 'date', date('Y-m-d')])
+            ->andWhere([
+                'BETWEEN',
+                new \yii\db\Expression("STR_TO_DATE(CONCAT(date, ' ', time), '%Y-%m-%d %H:%i:%s')"),
+                new \yii\db\Expression("DATE_ADD(NOW(), INTERVAL 119 MINUTE)"),
+                new \yii\db\Expression("DATE_ADD(NOW(), INTERVAL 121 MINUTE)")
+            ])
             ->all();
 
         foreach ($appointments as $appointment) {
@@ -52,6 +63,34 @@ class SendRemindersController extends Controller
         $count = count($appointments);
         Yii::info("Successfully queued {$count} 2-hour reminders.", 'jobs');
         $this->stdout("Successfully queued {$count} 2-hour reminders.\n", \yii\helpers\Console::FG_GREEN);
+    }
+
+    public function actionFindDueInHours($hours = 2, $toleranceMinutes = 1)
+    {
+        $startOffset = ($hours * 60) - $toleranceMinutes;
+        $endOffset = ($hours * 60) + $toleranceMinutes;
+        $appointments = Appointments::find()
+            //->where(['reminder_2hrs_sent' => NULL])
+            ->andWhere(['=', 'date', date('Y-m-d')])
+            ->andWhere([
+                'BETWEEN',
+                new \yii\db\Expression("STR_TO_DATE(CONCAT(date, ' ', time), '%Y-%m-%d %H:%i:%s')"),
+                new \yii\db\Expression("DATE_ADD(NOW(), INTERVAL {$startOffset} MINUTE)"),
+                new \yii\db\Expression("DATE_ADD(NOW(), INTERVAL {$endOffset} MINUTE)")
+            ])
+            ->all();
+
+        //return $appointments;
+        if (empty($appointments)) {
+            $this->stdout("No appointments found within the specified range.\n", \yii\helpers\Console::FG_YELLOW);
+        } else {
+            foreach ($appointments as $appointment) {
+                // Customize this output as needed
+                $this->stdout("Appointment ID: {$appointment->id}, Date: {$appointment->date}, Time: {$appointment->time}, Brief: {$appointment->symptoms_brief} \n", \yii\helpers\Console::FG_GREEN);
+            }
+        }
+
+        return ExitCode::OK; // Proper way to exit a Yii console action
     }
 
 
