04e11db328b365b4e1f588a64d85b23c3cda6e89
mwanginjamb <fnjambi@outlook.com>
2025-05-24 16:20:38 +0300

    Add linkedin App verification link

--- frontend/views/layouts/guest.php
+++ frontend/views/layouts/guest.php
@@ -108,6 +108,8 @@ AppAsset::register($this);
     </footer>
 
     <?php $this->endBody() ?>
+    <a href="https://www.linkedin.com/developers/apps/verification/979aa38d-01c6-454b-9f85-3d33729b951f
+"></a>
 </body>
 
 </html>
