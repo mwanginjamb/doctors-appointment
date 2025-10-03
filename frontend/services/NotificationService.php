0c7ce22d2966924a8ac1df5a0df8b6b6168f762d
mwanginjamb <fnjambi@outlook.com>
2025-10-03 16:07:25 +0300

    Refactor: maintain 2 job queues for patient and one for consultant

--- frontend/services/NotificationService.php
+++ frontend/services/NotificationService.php
@@ -286,7 +286,13 @@ class NotificationService
                 'appointmentId' => $appointment->id,
                 'reminderType' => 'immediate',
                 'notificationId' => null,
-                'recipientType' => NotificationSchedules::METHOD_BOTH
+                'recipientType' => NotificationSchedules::METHOD_PATIENT
+            ]));
+            Yii::$app->queue->push(new SendReminderEmailJob([
+                'appointmentId' => $appointment->id,
+                'reminderType' => 'immediate',
+                'notificationId' => null,
+                'recipientType' => NotificationSchedules::METHOD_CONSULTANT
             ]));
 
         } else {
@@ -303,7 +309,14 @@ class NotificationService
                 'appointmentId' => $appointment->id,
                 'reminderType' => 'reschedule',
                 'notificationId' => null,
-                'recipientType' => NotificationSchedules::METHOD_BOTH
+                'recipientType' => NotificationSchedules::METHOD_PATIENT
+            ]));
+
+            Yii::$app->queue->push(new SendReminderEmailJob([
+                'appointmentId' => $appointment->id,
+                'reminderType' => 'reschedule',
+                'notificationId' => null,
+                'recipientType' => NotificationSchedules::METHOD_CONSULTANT
             ]));
 
         } else {
