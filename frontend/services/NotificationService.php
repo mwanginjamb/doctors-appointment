dcbf1364b81a09fb1a0bd87e2ec642213ca1f0e5
mwanginjamb <fnjambi@outlook.com>
2025-10-03 21:59:57 +0300

    refactor: remove separate jobs for different recipient type

--- frontend/services/NotificationService.php
+++ frontend/services/NotificationService.php
@@ -286,14 +286,9 @@ class NotificationService
                 'appointmentId' => $appointment->id,
                 'reminderType' => 'immediate',
                 'notificationId' => null,
-                'recipientType' => NotificationSchedules::METHOD_PATIENT
+                'recipientType' => NotificationSchedules::METHOD_BOTH
             ]));
-            Yii::$app->queue->push(new SendReminderEmailJob([
-                'appointmentId' => $appointment->id,
-                'reminderType' => 'immediate',
-                'notificationId' => null,
-                'recipientType' => NotificationSchedules::METHOD_CONSULTANT
-            ]));
+
 
         } else {
             Yii::error('No patient email found for immediate confirmation notification for appointment ' . $appointment->id, 'notifications');
@@ -309,14 +304,7 @@ class NotificationService
                 'appointmentId' => $appointment->id,
                 'reminderType' => 'reschedule',
                 'notificationId' => null,
-                'recipientType' => NotificationSchedules::METHOD_PATIENT
-            ]));
-
-            Yii::$app->queue->push(new SendReminderEmailJob([
-                'appointmentId' => $appointment->id,
-                'reminderType' => 'reschedule',
-                'notificationId' => null,
-                'recipientType' => NotificationSchedules::METHOD_CONSULTANT
+                'recipientType' => NotificationSchedules::METHOD_BOTH
             ]));
 
         } else {
